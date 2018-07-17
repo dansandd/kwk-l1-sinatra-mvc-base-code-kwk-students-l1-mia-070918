443dc469999677e8fe344e9811669f26ffcd6400
dansandd <dana.sandler@gmail.com>
2018-07-17 15:26:41 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -2,9 +2,22 @@ require 'bundler'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
-  get '/' do
-    erb :index
+  get "/" do #when u run shotgun and copy paste the server number this will show
+    return erb:index
   end
 
+  post "/"do 
+    return erb:results
+  end
+  
+  get "/video" do #if u put /video to your server it will show page2
+    "page2"
+  end
+  
+  get "/folder" do
+  return erb:index #telling sinatra go to file named index 
+  end
+  
+ 
+  end
 end
